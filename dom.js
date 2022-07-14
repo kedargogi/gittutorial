7deb0aef0aeb7b2501dd740c90c576857763b16a
kedargogi <kedargogi420@gmail.com>
2022-07-14 21:29:10 +0530

    task3 commit

---
+++ dom.js
@@ -0,0 +1,8 @@
+//console.dir(document);
+//console.dir(document.URL);
+let headerTitle=document.getElementById('header-title');
+headerTitle.style.border='2px solid black';
+
+let title=document.querySelector('h2.title');
+title.innerHTML="<b> Add Item</b>"
+title.style.color='#00ff00';
