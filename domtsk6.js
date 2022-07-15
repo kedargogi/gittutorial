338cb22bc0fe51f8d3231171df0c7520f6ce1447
kedargogi <kedargogi420@gmail.com>
2022-07-15 17:46:41 +0530

    domtsk6 commit

---
+++ domtsk6.js
@@ -0,0 +1,10 @@
+//let secondItem= document.querySelector('ul li:nth-child(2)');
+//let thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+let odd =document.querySelectorAll('ul li:nth-child(odd)');
+for(let i=0;i<odd.length;i++){
+odd[i].style.backgroundColor = "#00ff00";
+}
+//secondItem.style.backgroundColor = "#00ff00";
+//thirdItem.style.visibility="hidden";
+
+//secondItem.style.color='green';
