d2cd3f8f150140e44a104fbfdb5ec9e9cbd8b974
kedargogi <kedargogi420@gmail.com>
2022-07-15 11:38:05 +0530

    domtsk4 commit

---
+++ domtsk4.js
@@ -0,0 +1,7 @@
+//let list=document.querySelector('ul li:nth-child(3)');
+let listElem = document.getElementsByTagName("li")[2];
+//listElem.style.backgroundColor = "#00ff00";
+
+//Make all the elements in the list have bold color font for some reason it doesn't work
+let listEle = document.getElementsByClassName("list-group");
+listEle.style.fontWeight = "bold";
