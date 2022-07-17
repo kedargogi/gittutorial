41e78e5a5160e65bfe22b6c9e9a4b555de171eda
kedargogi <kedargogi420@gmail.com>
2022-07-17 23:10:21 +0530

    domtsk11 commit

--- task10/script.js
+++ task10/script.js
@@ -3,6 +3,17 @@ let name,email;
 name=document.getElementById('name').value;
 email=document.getElementById('email').value;
 
-localStorage.setItem('name',name);
-localStorage.setItem('email',email);
+//save to local storage using object
+const user={
+    name:name,
+    email:email
+}
+//use json.stringify to convert into string else it shows obj_obj
+localStorage.setItem('user-copy',JSON.stringify(user));
+
+//to get obj back we use JSON.parse
+JSON.parse(localStorage.getItem('user-copy'));
+
+//localStorage.setItem('name',name);
+//localStorage.setItem('email',email);
 }
